c5cc39697841b0a62733661088222cbfb3226fc4
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-09-28 09:11:17 +0100

    Setting up store

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Hello world</Text>
       </View>
