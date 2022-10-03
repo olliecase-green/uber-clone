403e0ca770ac91d754a617485dce4ec66c699061
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-10-03 09:09:54 +0100

    HomeScreen component

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Hello world</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
