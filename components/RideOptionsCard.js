20eb8cf19863157738d871d46f8f9ea0fb1a4e2a
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-11-11 00:10:55 +0000

    Completed RideOptionsCard

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,23 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import tw from "twrnc";
-import { Text, View, SafeAreaView, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
 import { Icon } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
 
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_537/v1568070387/assets/b5/0a5191-836e-42bf-ad5d-6cb3100ec425/original/UberX.png",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_537/v1568134115/assets/6d/354919-18b0-45d0-a151-501ab4c4b114/original/XL.png",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image:
+      "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_537/v1569012915/assets/4f/599c47-7f5c-4544-a5d2-926babc8e113/original/Lux.png",
+  },
+];
+
 export default function RideOptionsCard() {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
 
   return (
-    <SafeAreaView style={tw`bg-white flex-gr\ow`}>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
       <View style={"z-index: 10"}>
         <TouchableOpacity
           onPress={() => navigation.navigate("NavigateCard")}
-          // onPress={() => console.log("hello")}
           style={tw`absolute top-3 left-5 p-3 rounded-full z-1`}
         >
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row items-center justify-between px-10 ${
+              id === selected?.id ? "bg-gray-200" : ""
+            }`}
+          >
+            <Image
+              style={{
+                width: 75,
+                height: 75,
+                resizeMode: "contain",
+              }}
+              source={{ url: image }}
+            />
+            <View style={tw`-ml-10`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>£99</Text>
+          </TouchableOpacity>
+        )}
+      />
+
+      <View>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black m-1 ${!selected ? "bg-gray-300" : ""}`}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
